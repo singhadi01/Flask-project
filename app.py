266598f3bbb06436a4a701aa163cf5193807a136
Aditya Singh <140547416+singhadi01@users.noreply.github.com>
2024-10-27 09:13:43 +0000

    added data and api route

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask,render_template 
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
+JOBS=[
+  {
+    'id': 1,
+    'title': 'Machine Operator',
+    'salary': "Rs. 12,500/m",
+  },
+  {
+    'id': 2,
+    'title': 'Delivery Agent',
+    'salary': "Rs. 10,000/m",
+  },
+  {
+    'id': 3,
+    'title': 'Engineer',
+    'salary': "Rs. 11,000/m",
+  }
+]
 @app.route('/')
 def hello_world(): 
-  return render_template('index.html')
+  return render_template('index.html',jobs=JOBS)
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
