11d34d67e8e5265e131ad356055659c4040a17de
Aditya Singh <140547416+singhadi01@users.noreply.github.com>
2024-10-27 19:34:12 +0000

    new changes db

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask,render_template,jsonify,request
+from flask_mysqldb import MySQl
+import yaml
+db = yaml.load(open('database.yaml'), Loader=yaml.FullLoader)
 app=Flask(__name__)
+db=yaml.load(open('database.yaml'))
+app.config['MYSQL_HOST']=db['mysql_host']
+app.config['MYSQL_USER']=db['mysql_user']
+app.config['MYSQL_PASSWORD']=db['mysql_password']
+app.config['MYSQL_DB']=db['mysql_db']
+mysql=MySQl(app)
+@app.route('/test_connection')
+def test_connection():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT VERSION()")
+        version = cur.fetchone()
+        cur.close()
+        return jsonify({"version": version[0]})
+    except Exception as e:
+        return jsonify({"error": str(e)})
 JOBS=[
   {
     'id': 1,
